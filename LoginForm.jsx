46d3e002bfc18df4679e711a33882cdde5f35299
Asdethprime <asdethprime@gmail.com>
2021-06-21 11:28:16 +1000

    Create LoginForm.jsx

---
+++ LoginForm.jsx
@@ -0,0 +1,51 @@
+    <section class="hero is-primary is-fullheight">
+      <main class="hero-body">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-5-tablet is-4-desktop">
+              <form class="box">
+                <div class="field has-text-centered">
+                  <img
+                    src="https://www.elegantthemes.com/blog/wp-content/uploads/2021/03/one-click-gmail-login-for-wordpress-featured-image.jpg"
+                  />
+                </div>
+                <div class="field">
+                  <label class="label">Email</label>
+                  <div class="control has-icons-left">
+                    <input
+                      class="input"
+                      type="email"
+                      placeholder="example@email.com"
+                    />
+                    <span class="icon is-left">
+                      <i class="fa fa-envelope"></i>
+                    </span>
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label">Password</label>
+                  <div class="control has-icons-left">
+                    <input
+                      class="input"
+                      type="password"
+                      placeholder="*******"
+                    />
+                    <span class="icon is-left">
+                      <i class="fa fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="checkbox">
+                    <input type="checkbox" /> Remember Me
+                  </label>
+                </div>
+                <div class="field">
+                  <button class="button is-success">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
